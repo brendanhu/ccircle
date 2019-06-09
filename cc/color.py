7ac7e2e89a53298dc3a71c03dab6eedbd62b5ed9
Brendan Hu <brendan.hu@berkeley.edu>
2019-06-08 18:13:59 -0700

    color.sRgb -> from255

--- cc/color.py
+++ cc/color.py
@@ -1,6 +1,6 @@
 class Color:
     """ A (linear RGB 0-1) color with an optional alpha.
-        Creation via sRGB values (0-255) is supported via Color#sRgb().
+        Creation via sRGB values (0-255) is supported via Color#from255().
     """
 
     def __init__(self, r: float, g: float, b: float, a: float = 1.0):
@@ -10,7 +10,7 @@ class Color:
         self.a = a
 
     @staticmethod
-    def sRgb(r: int, g: int, b: int, a: float = 1.0):
+    def from255(r: int, g: int, b: int, a: float = 1.0):
         """ Create a color via sRGB values. Alpha should still be within [0.0, 1.0]"""
         return Color(
             r / 255.0,
